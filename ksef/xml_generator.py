c143187889bbb913bf62e30f77ab0c2ddcf8b48b
kwasiucionek <kwasiek2gail.com>
2026-01-12 14:11:48 +0100

    updated files

--- ksef/xml_generator.py
+++ ksef/xml_generator.py
@@ -1,6 +1,5 @@
 # kwasiucionek/moja-fakturownia/moja-fakturownia-a0f550ee045da0fa60a613fb7a8884b3052e00a0/ksef/xml_generator.py
 
-from ksiegowosc.models import Invoice
 from datetime import datetime
 from decimal import Decimal
 
